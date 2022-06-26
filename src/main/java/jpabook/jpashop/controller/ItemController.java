9353177008ccabd727cb0e84d45ece9da14f8698
mamulee <ghdah7@gmail.com>
2022-06-26 23:40:34 +0900

    상품 등록

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        // Order처럼 생성자 가지고 Setter 없이 생성하는 게 좋은 설계이다. 지금은 편의상 이렇게.
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
